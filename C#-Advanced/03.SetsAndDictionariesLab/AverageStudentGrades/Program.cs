c7d3490ff070468e9965c382562f05580425e7c4
Petar Yankov <petar741012@gmail.com>
2021-01-27 00:36:13 +0200

    Added Sets and Dictionaries exercises from Lab

---
+++ C#-Advanced/03.SetsAndDictionariesLab/AverageStudentGrades/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] currStudent = Console.ReadLine().Split();
+                string name = currStudent[0];
+                decimal grade = decimal.Parse(currStudent[1]);
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<decimal>());
+                }
+                students[name].Add(grade);
+            }
+
+            foreach (var student in students)
+            {
+                Console.Write($"{student.Key} -> ");
+
+                foreach (var grade in student.Value)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+                Console.WriteLine($"(avg: {student.Value.Average():f2})");
+            }
+        }
+    }
+}
---
+++ C#-Advanced/03.SetsAndDictionariesLab/CountSameValuesInArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Dictionary<double, int> numbers = new Dictionary<double, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!numbers.ContainsKey(input[i]))
+                {                    
+                    numbers.Add(input[i], 1);
+                    continue;
+                }
+                numbers[input[i]]++;
+            }
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+        }
+    }
+}
