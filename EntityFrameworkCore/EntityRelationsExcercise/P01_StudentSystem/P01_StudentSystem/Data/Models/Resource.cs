439b78ed9df7f0ed88ffd1e6d8f5fcb34ee20305
petaryankov00 <petar741012@gmail.com>
2021-11-02 14:40:19 +0200

    Upload StudentSystemDB

---
+++ EntityFrameworkCore/EntityRelationsExcercise/P01_StudentSystem/P01_StudentSystem/Data/Models/Resource.cs
@@ -0,0 +1,27 @@
+﻿using P01_StudentSystem.Data.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Resource
+    {
+        public int ResourceId { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Url { get; set; }
+
+        public ResourceTypes ResourceType { get; set; }
+
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
---
+++ EntityFrameworkCore/EntityRelationsExcercise/P01_StudentSystem/P01_StudentSystem/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data;
+using P01_StudentSystem.Data.Models;
+using System;
+
+namespace P01_StudentSystem
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new StudentSystemContext();
+
+            db.Database.Migrate();
+
+            Console.WriteLine("Database creation succesfull");
+
+        }
+    }
+}
