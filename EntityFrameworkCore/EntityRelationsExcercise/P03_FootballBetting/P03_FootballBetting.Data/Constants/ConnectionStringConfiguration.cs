498340fa44de634a6b4c4919ce522e750f084656
petaryankov00 <petar741012@gmail.com>
2021-11-02 14:42:42 +0200

    EF Code First

---
+++ EntityFrameworkCore/EntityRelationsExcercise/P03_FootballBetting/P03_FootballBetting.Data/Constants/ConnectionStringConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Constants
+{
+    public class ConnectionStringConfiguration
+    {
+        protected internal const string UserId = "sa";
+
+        protected internal const string Password = "1916";
+
+        
+    }
+}
