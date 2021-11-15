72c9fb645140efb493ce4b791be09cca628c1aac
petaryankov00 <petar741012@gmail.com>
2021-11-15 10:17:28 +0200

    JSON

---
+++ EntityFrameworkCore/JSONProcessing/ProductShop/ProductShop/ModelsDTO/ProductInputDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductShop.ModelsDTO
+{
+    public class ProductInputDto
+    {
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int SellerId { get; set; }
+
+        public int? BuyerId { get; set; }
+
+    }
+}
---
+++ EntityFrameworkCore/JSONProcessing/ProductShop/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ProductShop.Models;
+using ProductShop.ModelsDTO;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            this.CreateMap<UserInputDto, User>();
+            this.CreateMap<ProductInputDto, Product>();
+            this.CreateMap<CategoryInputDto, Category>();
+            this.CreateMap<CategoryProductsInputDto, CategoryProduct>();
+            this.CreateMap<User, UsersOutputModelDto>();
+                
+        }
+    }
+}
