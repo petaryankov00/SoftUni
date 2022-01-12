00cd645c52e4bbdef2e256bbe17521ddff25cd94
petaryankov00 <petar741012@gmail.com>
2022-01-12 16:01:48 +0200

    MergeSort

---
+++ Algorithms/MergeSort/MergeSort.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    public class MergeSort<T>
+        where T : IComparable<T>
+    {
+        private static T[] temp;
+
+        public static void Sort(T[] array, int lo, int hi)
+        {
+            if (lo >= hi)
+            {
+                return;
+            }
+
+            int mid = lo + (hi - lo) / 2;
+            Sort(array, lo, mid);
+            Sort(array, mid + 1, hi);
+
+            Merge(array, lo, mid, hi);
+
+
+
+        }
+
+        private static void Merge(T[] arr, int lo, int mid, int hi)
+        {
+
+            if (IsLess(arr[mid], arr[mid + 1]))
+            {
+                return;
+            }
+
+            temp = new T[arr.Length];
+
+            for (int i = lo; i <= hi; i++)
+            {
+                temp[i] = arr[i];
+            }
+
+            int n1 = lo;
+            int n2 = mid + 1;
+
+
+            for (int i = lo; i <= hi; i++)
+            {
+                if (n1 > mid)
+                {
+                    arr[i] = temp[n2++];
+                }
+                else if (n2 > hi)
+                {
+                    arr[i] = temp[n1++];
+                }
+                else if (IsLess(temp[n1],temp[n2]))
+                {
+                    arr[i] = temp[n1++];
+                }
+                else
+                {
+                    arr[i] = temp[n2++];
+                }
+            }
+
+        }
+
+        private static bool IsLess(T firstElement,T secondElement)
+        {
+            if (firstElement.CompareTo(secondElement) < 0)
+            {
+                return true;
+            }
+            return false;
+                    
+        }
+    }
+}
---
+++ Algorithms/MergeSort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace MergeSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[100000];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = 1000 - i;
+            }
+            var watch = new Stopwatch();
+
+            watch.Start();
+            MergeSort<int>.Sort(array,0,array.Length - 1);
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+
+            watch.Start();
+            array.OrderBy(x => x);
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+
+            
+        }
+    }
+}
