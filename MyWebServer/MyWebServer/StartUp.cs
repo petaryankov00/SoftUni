ca704271776b54c29bec98790798b2477624142a
petaryankov00 <petar741012@gmail.com>
2022-01-13 16:43:04 +0200

    CustomWebServer

---
+++ MyWebServer/MyWebServer/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using MyWebServer.Server;
+
+namespace MyWebServer.Demo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var server = new HttpServer("127.0.0.1", 8080);
+            server.Start();
+        }
+    }
+}
