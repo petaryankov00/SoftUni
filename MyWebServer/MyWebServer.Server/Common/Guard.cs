216ed4d1a96b7544fcdff170cf2ff559d9ff8cc7
petaryankov00 <petar741012@gmail.com>
2022-01-13 17:14:35 +0200

    CustomWebServer

---
+++ MyWebServer/MyWebServer.Server/Common/Guard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebServer.Server.Common
+{
+    public class Guard
+    {
+        public static void AgaintsNull(object value, string name = null)
+        {
+            if (value == null)
+            {
+                name ??= "Value";
+
+                throw new ArgumentException($"{name} cannot be null.");
+            }
+        }
+    }
+}
---
+++ MyWebServer/MyWebServer.Server/HTTP/HeaderCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWebServer.Server.HTTP
+{
+    public class HeaderCollection
+    {
+        private readonly Dictionary<string, Header> headers = new Dictionary<string, Header>();
+
+        public int Count => this.headers.Count;
+
+        public void Add(string name, string value)
+        {
+            var currHeader = new Header(name, value);
+
+            this.headers.Add(name, currHeader);
+        }
+
+    }
+}
