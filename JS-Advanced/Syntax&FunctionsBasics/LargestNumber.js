bf700e435611d925a5d79e18705f0f8fcd1632b9
petaryankov00 <petar741012@gmail.com>
2022-01-23 22:55:40 +0200

    FunctionsJS

---
+++ JS-Advanced/Syntax&FunctionsBasics/LargestNumber.js
@@ -0,0 +1,5 @@
+function solve(a,b,c){
+    console.log(`The largest number is ${Math.max(a,b,c)}.`);
+}
+
+solve(2,5,1225);
---
+++ JS-Advanced/Syntax&FunctionsBasics/MathOperations.js
@@ -0,0 +1,14 @@
+function solve(num1,num2,sign){
+    let result;
+    switch (sign) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
+        case '%': result = num1 % num2; break;
+        case '**': result = num1 ** num2; break;
+    }
+    return result;
+}
+
+console.log(solve(5,6,'+'));
