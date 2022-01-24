6788bd29d1010535ddd2fead64e85e0d5ec9c271
petaryankov00 <petar741012@gmail.com>
2022-01-24 15:55:06 +0200

    Fixed namespaces

---
+++ MyWebServer/MyWebServer.Server/HTTP/Cookie.cs
@@ -0,0 +1,18 @@
+﻿namespace MyWebServer.Server.HTTP
+{
+    public class Cookie
+    {
+        public Cookie(string _name, string _value)
+        {
+            this.Name = _name;
+            this.Value = _value;
+        }
+
+        public string Name { get; init; }
+
+        public string Value { get; init; }
+
+        public override string ToString()
+         => $"{this.Name}={this.Value}";
+    }
+}
---
+++ MyWebServer/MyWebServer.Server/Responses/ViewResponse.cs
@@ -0,0 +1,53 @@
+﻿using MyWebServer.Server.HTTP;
+
+namespace MyWebServer.Server.Responses
+{
+    public class ViewResponse : ContentResponse
+    {
+        private const char separator = '/';
+
+        public ViewResponse(string viewName, string controllerName, object model = null) 
+            : base("", ContentType.Html) 
+        {
+            if (!viewName.Contains(separator))
+            {
+                viewName = controllerName + separator + viewName;
+            }
+
+            var viewPath = Path.GetFullPath(
+                $"./Views/" + viewName.TrimStart(separator) + ".cshtml");
+
+            var viewContent = File.ReadAllText(viewPath);
+
+            if (model != null)
+            {
+                viewContent = this.PopulateModel(viewContent, model);
+            }
+
+            this.Body = viewContent; 
+        }
+
+        private string PopulateModel(string viewContent, object model)
+        {
+            var data = model
+                .GetType()
+                .GetProperties()
+                .Select(x => new
+                {
+                    x.Name,
+                    Value = x.GetValue(model)
+                });
+
+            foreach (var item in data)
+            {
+                const string openingBrackets = "{{";
+                const string closingBrackets = "}}";
+
+                viewContent = viewContent.Replace($"{openingBrackets}{item.Name}{closingBrackets}",
+                    item.Value.ToString());
+            }
+
+            return viewContent;
+        }
+    }
+}
